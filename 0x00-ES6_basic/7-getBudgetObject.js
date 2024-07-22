099f6d7f5cd7306efb031364a7d768b67253d800
bravin-onwonga <onbravin@gmail.com>
2024-07-23 00:24:58 +0300

    fix:Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,12 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const arg1 = 'income';
-  const arg2 = 'gdp';
-  const arg3 = 'capita';
-
   const budget = {
-    [arg1]: income,
-    [arg2]: gdp,
-    [arg3]: capita,
+    income,
+    gdp,
+    capita
   };
 
   return budget;
