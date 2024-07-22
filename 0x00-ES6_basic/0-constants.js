eae29b07ede811a76be9db5335076cc05449666c
bravin-onwonga <onbravin@gmail.com>
2024-07-22 21:08:33 +0300

    task:Using const rather than var

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
