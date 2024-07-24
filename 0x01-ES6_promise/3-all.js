36dfa94e516144f84cf62b81e3b1b1955314fd88
bravin-onwonga <onbravin@gmail.com>
2024-07-25 00:39:36 +0300

    task:Handle several function returnung promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    })
+    .catch((err) => {
+      console.log(new Error('Signup system offline', err));
+    });
+}
