2a1b0f7e2bacb298bef43ed5fad7b06d7374a7b9
bravin-onwonga <onbravin@gmail.com>
2024-07-26 02:37:42 +0300

    fix:Using computed property name

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,11 +1,9 @@
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
   const year = new Date().getFullYear();
 
-  budget[`income-${year}`] = income;
-  budget[`gdp-${year}`] = gdp;
-  budget[`capita-${year}`] = capita;
-
-  return budget;
+  return {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
+  };
 }
