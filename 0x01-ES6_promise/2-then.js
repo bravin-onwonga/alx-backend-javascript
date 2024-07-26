c14112ba3c972a357fb9121413b92b01aeef0fa4
bravin-onwonga <onbravin@gmail.com>
2024-07-26 20:45:38 +0300

    fix:Returning a new promise executed after the promise is resolved or rejected

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then((success) => {
       console.log('Got a response from the API');
-      resolve({ status: 200, body: success });
+      return ({ status: 200, body: success });
     }).catch(() => {
       reject(new Error());
     });
