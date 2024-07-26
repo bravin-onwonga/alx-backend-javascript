a3e9783303cfa388660c0411bd354b94f1113c31
bravin-onwonga <onbravin@gmail.com>
2024-07-26 21:28:45 +0300

    fix:Not returning a new promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then((success) => {
-      resolve({ status: 200, body: success });
-    }).catch(() => {
-      reject(new Error());
-    }).finally(() => {
-      console.log('Got a response from the API');
-    })
+  promise.then((success) => {
+    return ({ status: 200, body: success });
+  }).catch(() => {
+    reject(new Error());
+  }).finally(() => {
+    console.log('Got a response from the API');
   });
 }
