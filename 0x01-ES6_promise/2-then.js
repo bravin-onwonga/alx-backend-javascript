fc0c0d7828b380cbe063fcfad27361ffafa7a41f
bravin-onwonga <onbravin@gmail.com>
2024-07-26 21:02:37 +0300

    Adding finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then((success) => {
-      console.log('Got a response from the API');
-      return ({ status: 200, body: success });
+      resolve({ status: 200, body: success });
     }).catch(() => {
       reject(new Error());
-    });
+    }).finally(() => {
+      console.log('Got a response from the API');
+    })
   });
 }
