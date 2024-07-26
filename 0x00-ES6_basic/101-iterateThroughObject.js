49341d27a9ba48c4fe4161436007a8636cf96552
bravin-onwonga <onbravin@gmail.com>
2024-07-26 04:37:50 +0300

    task:Converting array to string

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let idx = 0;
-  let txt = '';
-
-  for (const name of reportWithIterator) {
-    txt += name;
-    if (idx < reportWithIterator.length - 1){
-      txt += '| ';
-    }
-  }
+  return reportWithIterator.join(' | ');
 }
