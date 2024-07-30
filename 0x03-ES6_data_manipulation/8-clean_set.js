b70d4d8068cf1582ed83e0e2aace329c3556d9d0
bravin-onwonga <onbravin@gmail.com>
2024-07-30 14:51:44 +0300

    task:Checking if startString is empty

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
   let myStr = '';
 
+  if (startString.length === 0) {
+    return myStr;
+  }
+
   set.forEach((element) => {
-    if (element.startsWith(startString) && startString.length !== 0) {
+    if (element.startsWith(startString)) {
       const ele = element.slice(startString.length);
       if (myStr.length !== 0) {
         myStr += '-' + ele;
