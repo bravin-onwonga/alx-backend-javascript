ea22e402a01d8422ea15e0e9c98fb66630a0d1e8
bravin-onwonga <onbravin@gmail.com>
2024-07-30 18:49:16 +0300

    Refactoring my code

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,7 @@
-let count = 0;
-
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
   count += 1;
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
-  }
   weakMap.set(endpoint, count);
 }
