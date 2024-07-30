3cd8412aad48f7e79ba490fcda732a2e429d0d3b
bravin-onwonga <onbravin@gmail.com>
2024-07-30 13:28:04 +0300

    fix:Creating a buffer then a view

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,6 @@
 export default function createInt8TypedArray(length, position, Number) {
-  const int8Arr = new Int8Array(length);
+  const buffer = new ArrayBuffer(length)
+  const int8Arr = new Int8Array(buffer);
 
   try {
     int8Arr[position] = Number;
@@ -8,5 +9,5 @@ export default function createInt8TypedArray(length, position, Number) {
     throw new Error('Position outside range');
   }
 
-  return int8Arr;
+  return buffer;
 }
