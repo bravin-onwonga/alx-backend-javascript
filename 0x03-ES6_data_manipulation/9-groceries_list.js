6895a1c22db08aa85de2a9d014817b20dcee7a09
bravin-onwonga <onbravin@gmail.com>
2024-07-30 15:00:09 +0300

    task:Creating a map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const myMapp = new Map();
+
+  myMapp.set('Apples', 10);
+  myMapp.set('Tomatoes', 10);
+  myMapp.set('Pasta', 1);
+  myMapp.set('Rice', 1);
+  myMapp.set('Banana', 5);
+
+  return myMapp;
+}
