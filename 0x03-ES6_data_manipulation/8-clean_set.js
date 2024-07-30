7e258493b83e9de35f8b07095192a51456fe7811
bravin-onwonga <onbravin@gmail.com>
2024-07-30 17:53:21 +0300

    fix:Adding more checks and edge cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   let myStr = '';
 
-  if (startString.length === 0) {
+  if (startString.length === 0 || (typeof startString !== 'string')) {
     return myStr;
   }
 
   set.forEach((element) => {
-    if (element.startsWith(startString)) {
+    if (typeof element === 'string' && element.startsWith(startString)) {
       const ele = element.slice(startString.length);
       if (myStr.length !== 0) {
         myStr += `-${ele}`;
