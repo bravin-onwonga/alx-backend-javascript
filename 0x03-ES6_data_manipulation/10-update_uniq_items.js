ddfa0e0f5a6524358ad7d67035b1af08fb31da9b
bravin-onwonga <onbravin@gmail.com>
2024-07-30 17:21:37 +0300

    fix:Throw error if it is not a map object

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
 export default function updateUniqueItems(map) {
+  if (typeof map !== 'object' || Array.isArray(map)) {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key) => {
     if (value === 1) {
-      try {
-        map.set(key, 100);
-      } catch {
-        throw new Error('Cannot process', err);
-      }
+      map.set(key, 100);
     }
   });
 }
