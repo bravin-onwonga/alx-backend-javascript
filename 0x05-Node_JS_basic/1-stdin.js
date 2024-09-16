8fb5cd946044282bdbf0b1cc344fb530f715b734
bravin-onwonga <onbravin@gmail.com>
2024-09-16 15:02:28 +0300

    Using process to read from and write to terminal

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('readable', function () {
   const name = process.stdin.read();
   if (name !== null) {
