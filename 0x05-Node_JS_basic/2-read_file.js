9f2bb726e09ed9f32141a35e1bacedc28e45e668
bravin-onwonga <onbravin@gmail.com>
2024-09-16 16:28:31 +0300

    Read from file synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const countStudents = (filename) => {
+  try {
+    if (filename !== null) {
+      const data = fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
+
+      if (data) {
+        const arr = Array.from(data.split('\n'));
+        let i = 1;
+        const myObj = {};
+        while (arr[i]) {
+          const studentDetails = Array.from(arr[i].split(','));
+          const course = studentDetails[3];
+          if (myObj[course]) {
+            myObj[course].push(studentDetails[0]);
+          } else {
+            myObj[course] = [studentDetails[0]];
+          }
+          i++;
+        }
+        let numberOfStudents = 0;
+        for (const element in myObj) {
+          numberOfStudents = numberOfStudents + myObj[element].length;
+        }
+        console.log('Number of students: ' + numberOfStudents);
+        for (const ele in myObj) {
+          const studentsInCourse = myObj[ele].length;
+          console.log(`Number of students in ${ele}: ${studentsInCourse}. List: ${myObj[ele].join(', ')}`);
+        }
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
