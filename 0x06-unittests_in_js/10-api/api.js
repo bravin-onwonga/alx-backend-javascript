348dc4ef603035106b355dbfa5e46202468b323a
bravin-onwonga <onbravin@gmail.com>
2024-10-25 23:32:10 +0300

    fix: Making sure all routes are working well

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const app = express();
-const PORT = 7865;
+const port = 7865;
 
 app.get('/', (req, res) => res.end('Welcome to the payment system'))
 app.get('/cart/:id([0-9]+)', (req, res) => {
@@ -19,4 +19,5 @@ app.get('/available_payments', (req, res) => {
 app.post('/login', (req, res) => {
   res.end(`Welcome ${req.body.userName}`);
 });
-app.listen(PORT, () => console.log(`API running on port: ${PORT}`));
+
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
